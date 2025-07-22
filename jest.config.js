aa51abc35b3295a948c93e2c2d0c53018a41298b
Ash <ashswow@hotmail.co.uk>
2025-07-22 13:29:31 +0100

    Refactored codebase, fixed tests and TS errors

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-    dir: './'
+  dir: './',
 });
 
 const customJestConfig = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    moduleNameMapper: {'^@/(.*)$': '<rootDir>/src/$1'},
-    testEnvironment: 'jest-environment-jsdom'
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+  testEnvironment: 'jest-environment-jsdom',
 };
 
 module.exports = createJestConfig(customJestConfig);
---
+++ src/__tests__/app/home.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
+import Home from '@app/page';
+
+describe('Home', () => {
+  beforeEach(() => {
+    render(<Home />);
+  });
+
+  it('renders an empty basket', () => {
+    const basketButton = screen.getByRole('button', {
+      name: /Basket:/i,
+    });
+
+    expect(basketButton).toHaveTextContent('Basket: 0 items');
+  });
+
+  it('renders a basket with 1 item', async () => {
+    const buttons = screen.getAllByRole('button', {
+      name: /Add to basket/i,
+    });
+
+    await act(async () => {
+      await buttons[0].click();
+    });
+
+    const basketButton = screen.getByRole('button', {
+      name: /Basket:/i,
+    });
+
+    expect(basketButton).toHaveTextContent(/Basket: 1 items$/);
+  });
+
+  it('renders a basket with 1 of item 1 and 2 of item 2', async () => {
+    const buttons = screen.getAllByRole('button', {
+      name: /Add to basket/i,
+    });
+
+    await act(async () => {
+      await buttons[0].click();
+      await buttons[1].click();
+      await buttons[1].click();
+    });
+
+    const basketButton = screen.getByRole('button', {
+      name: /Basket:/i,
+    });
+
+    expect(basketButton).toHaveTextContent(/Basket: 3 items$/);
+  });
+});
