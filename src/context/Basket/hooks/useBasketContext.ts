f72420de0c031ba6ca86a97e2f93a715a9711d0f
Ash <ashswow@hotmail.co.uk>
2025-07-23 01:04:11 +0100

    Use @context import

--- src/context/Basket/hooks/useBasketContext.ts
+++ src/context/Basket/hooks/useBasketContext.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
 
-import { BasketContext } from '@/context/Basket';
+import { BasketContext } from '@context/Basket';
 
 export const useBasketContext = () => useContext(BasketContext);
